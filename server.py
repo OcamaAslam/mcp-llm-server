01c70812fa73006e25c6f920b9f1068f8dbfac31
Osama <osamabinaslam0@gmail.com>
2026-04-03 10:23:03 +0500

    define tools for llms

---
+++ server.py
@@ -0,0 +1,63 @@
+from mcp.server.fastmcp import FastMCP
+
+
+# create a server
+
+mcp = FastMCP("Notes Server", version="1.0.0")
+
+# to store notes
+notes = {}
+
+# define tools to be called by LLM
+
+@mcp.tool()
+def add_note(title, content):
+    """
+    Save a new note with a title and content.
+    Use this when the user wants to remember something.
+    """
+
+    if title not in notes:
+        notes[title] = content
+        return f"Note {title} saved successfully!"
+    
+    return "A note with similar title already exist"
+
+@mcp.tool()
+def del_note(title):
+
+    """
+    Delete a note by its title.
+    """
+
+    if title not in notes:
+        return "Error! title not found"
+    
+    del notes[title]
+
+    return f"Note {title} deleted successfully!"
+
+@mcp.tool()
+def search_notes(query):
+
+    """
+    Search all notes for a keyword.
+    Returns matching note titles and their content.
+    """
+
+    res = []
+
+    if not notes:
+        return "No notes saved yet"
+    
+    for title, content in notes.items():
+        if query.lower() in title or query.lower() in content:
+            res.append(f"{title}: {content}")
+
+    if not res:
+        return "No notes found matching"
+    
+    return "\n".join(res)
+
+
+    
